5934d543ca6b16061b392660aa2e28160f25b7f5
natehall329 <nate.hall329@gmail.com>
2023-06-12 11:51:03 -0400

    switch execute_log to calling handlers framework

--- R/execute_log.R
+++ R/execute_log.R
@@ -1,54 +1,64 @@
-#' Execute R Code with Error/Warning Logging
+#' Execute a block of code and log the output
 #'
-#' This function executes an R expression or block of code, logging errors and warnings.
-#' It optionally logs output to a specified file and stores the output in a RDS file.
+#' This function takes a block of code, executes it, and logs the output, along with any warnings or errors.
+#' It can optionally also measure the runtime of the code block and save the output to a file.
 #'
-#' @param code An R expression or block of code to be executed. Typically a function of an analytic pipeline.
-#' @param log_path Character string. Path to the log file where errors and warnings are to be written.
-#' If NULL (default), output is written to the console.
-#' @param output_path Character string. Path to the RDS file where the output of the code is to be stored.
-#' If NULL (default), the output is not stored.
-#' @param describe_text Character string. A description text to include in the log output.
-#' If NULL (default), no description text is added.
+#' @param code The code block to execute. This should be a function or other executable R object.
+#' @param log_path (Optional) A character string specifying the path to the file where the log should be written.
+#'                If NULL (the default), no log file is created.
+#' @param output_path (Optional) A character string specifying the path to the file where the output should be saved.
+#'                   If NULL (the default), the output is not saved to a file.
+#' @param describe_text (Optional) A character string that will be printed before the code execution.
+#'                     It's typically used to describe the purpose or function of the code block.
+#'                     If NULL (the default), no descriptive text is printed.
+#' @param display_runtime (Optional) A logical value indicating whether the runtime of the code block should be printed.
+#'                       If TRUE (the default), the runtime is printed.
 #'
-#' @return If there is an error or warning during code execution, a `try-error` or `simpleWarning` object is returned,
-#' respectively. If the code executes successfully, the output of the code is returned.
+#' @return The output of the executed code block. If `output_path` is provided, the output is also saved to the specified file.
 #'
 #' @examples
 #' \dontrun{
-#'   execute_log({print("Hello, world!")}, log_path = "my_log.txt")
+#' execute_log({print("Hello, World!")}, log_path = "log.txt", output_path = "output.txt",
+#'             describe_text = "Printing a greeting", display_runtime = TRUE)
 #' }
-#'
-#' @author Nate Hall
 #'
 #' @export
 
-execute_log <- function(code, log_path = NULL, output_path = NULL, describe_text = NULL) {
+execute_log <- function(code, log_path = NULL, output_path = NULL, describe_text = NULL, display_runtime = TRUE) {
+
+  if(display_runtime) {tictoc::tic("Run time")}
+
   if (!is.null(log_path)) {
     log_conn <- file(log_path, open = "wt")
     sink(log_conn)
     sink(log_conn, type = "message")
+    print(bannerCommenter::boxup("Begin Execution"))
   }
 
-  o <- tryCatch(code, error = function(c) {
+  o <- withCallingHandlers({
+    result <- code
+    result
+  }, error = function(c) {
     if (is.null(describe_text)) {
-      cat("ERROR (", gsub("Error: ", "", gsub("\\n", "", as.character(c))), ")\n", sep = "")
+      cat("ERROR (", gsub("Error: ", "", gsub("\\n", "", as.character(c$message))), ")\n", sep = "")
     } else {
-      cat(describe_text, " ERROR (", gsub("Error: ", "", gsub("\\n", "", as.character(c))), ")\n", sep = "")
+      cat(describe_text, " ERROR (", gsub("Error: ", "", gsub("\\n", "", as.character(c$message))), ")\n", sep = "")
     }
-    return(c)
   }, warning = function(c) {
     if (is.null(describe_text)) {
-      cat("WARNING (", gsub("simpleWarning: ", "", gsub("\\n", "", as.character(c))), ")\n", sep = "")
+      cat("WARNING (", gsub("simpleWarning: ", "", gsub("\\n", "", as.character(c$message))), ")\n", sep = "")
     } else {
-      cat(describe_text, " WARNING (", gsub("simpleWarning: ", "", gsub("\\n", "", as.character(c))), ")\n", sep = "")
+      cat(describe_text, " WARNING (", gsub("simpleWarning: ", "", gsub("\\n", "", as.character(c$message))), ")\n", sep = "")
     }
-    return(c)
   })
 
+  print(bannerCommenter::boxup("Execution Output and Messages"))
+
   if (!any(c("error", "warning") %in% class(o)) && !is.null(describe_text)) {
-    cat("\n", describe_text, ": SUCCESS\n", sep = "")
+    cat("\n", describe_text, ": DONE\n", sep = "")
   }
+
+  print(bannerCommenter::boxup("Logging and Cleanup"))
 
   if (!is.null(output_path)) {
     output_objects <- list(
@@ -59,6 +69,8 @@ execute_log <- function(code, log_path = NULL, output_path = NULL, describe_text
   } else {
     output_objects <- o
   }
+
+  if(display_runtime) {tictoc::toc()}
 
   if (!is.null(log_path)) {
     sink()
